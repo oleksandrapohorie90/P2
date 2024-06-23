b6f0a996616d5467f193db8d3f8125ea17a48920
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-06-23 12:39:32 -0400

    6/23/24

---
+++ src/CS_Interpreters_Parsers/AssignmentNode.java
@@ -0,0 +1,18 @@
+package CS_Interpreters_Parsers;
+
+public class AssignmentNode extends ASTNode{
+    private String variable;
+    private ASTNode value;
+
+    public AssignmentNode(String variable, ASTNode value) {
+        this.variable = variable;
+        this.value = value;
+    }
+
+    @Override
+    public void print(String indent) {
+        System.out.println(indent+ "AssignmentNode{" +
+                variable + '=' + value +
+                '}');
+    }
+}
