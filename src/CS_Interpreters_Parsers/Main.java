2f7077152bf92ebd74443f286fdd8141a234dd6c
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-06-23 14:16:59 -0400

    6/23/24

--- src/CS_Interpreters_Parsers/Main.java
+++ src/CS_Interpreters_Parsers/Main.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParserException {
+        /**
+         * primary version:
+         * BinaryOp{+}
+         *     NumberNode{3}
+         *     BinaryOp{*}
+         *       NumberNode{5}
+         *       BinaryOp{-}
+         *         NumberNode{10}
+         *         NumberNode{4}
+         */
 
         List<Token> tokens = Lexer.getHardcodedTokens();
         Parser parser = new Parser(tokens);
